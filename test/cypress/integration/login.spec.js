7fb0119c93704d9ea3e7038794c46818777b4f44
julianv8 <juliandjv@gmail.com>
2018-11-22 15:00:27 -0500

    Acert, librerias chai, mocha y uso de fixture

--- test/cypress/integration/login.spec.js
+++ test/cypress/integration/login.spec.js
@@ -1,7 +1,38 @@
 'use strict'
 
 describe('Prueba del login', () => {
-  it('Debe cargar el login', () => {
+  beforeEach(() => {
+    cy.fixture('user.json').as('userData')
     cy.visit('/login')
+    cy.contains('h1', 'Bienvenido').should('be.visible')
+  })
+
+  it.skip('Debe registrar un usuario', () => {
+    cy.get('@userData').then((userData) => {
+      cy.contains('Crear una cuenta').click()
+      cy.get('#name').type(userData.name)
+      cy.get('#title').type(userData.company)
+      cy.get('#email2').type(userData.email)
+      cy.get('#password2').type(userData.password)
+      cy.contains('.button', 'Registrarse').click()
+      cy.wait(3000)
+      cy.get('.error-msg').should('not.exist')
+    })
+  })
+  it('Debe fallar el logueo usuario erroneo', () => {
+    cy.get('#email1').type('fail@algo.com')
+    cy.get('#password1').type('123456')
+    cy.contains('.button', 'Ingresar').click()
+    cy.wait(3000)
+    cy.get('.error-msg').should('be.visible')
+  })
+  it('Debe loguear el usuario', () => {
+    cy.get('@userData').then((userData) => {
+      cy.get('#email1').type(userData.email)
+      cy.get('#password1').type(userData.password)
+      cy.contains('.button', 'Ingresar').click()
+      cy.wait(3000)
+      cy.contains('a', 'Dashboard').should('be.visible')
+    })
   })
 })
